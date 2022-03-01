fa3c6923a53d85f7f356b6b0b8501dac9bf0e9b0
EstebanRincon12 <esteban.rincon@uptc.edu.co>
2022-03-01 13:20:10 -0500

    Project_added

---
+++ script/enlazadas.js
@@ -0,0 +1,107 @@
+console.log("Script Lista")
+function Node (value) {
+  this.value = value
+  this.next = null
+}
+
+function LinkedList() {
+  this.head = null
+}
+
+LinkedList.prototype.append = function(value, current = this.head){
+  if(this.head === null){
+    return this.head = new Node(value)
+  }
+  if(current.next === null){
+    return current.next = new Node(value)
+  }
+  this.append(value, current.next)
+}
+
+LinkedList.prototype.removeNode = function (value, current = this.head) {
+  if(this.head === null){ // no head
+    return false
+  }
+
+  if (this.head.value === value){
+    return this.head = this.head.next
+  }
+
+  if(current.next !== null){
+    if(current.next.value === value){
+      return current.next = current.next.next
+    }
+    this.removeNode(value, current.next)
+  }
+  return false // no match found
+}
+
+LinkedList.prototype.findNode = function (value, current = this.head){
+  if(this.head === null) {
+    return false
+  }
+
+  if (current !== null) {
+    if (current.value === value){
+      return true
+    } else {
+      return this.findNode(value, current.next)
+    }
+  }
+  return false
+}
+
+LinkedList.prototype.peekNode = function (value) {
+  if(this.head === null) {
+    return false
+  }
+  return this.head
+}
+
+
+LinkedList.prototype.length = function (current = this.head, acum = 1) {
+  if(this.head === null){
+    return 0
+  }
+  if (current.next !== null){
+    return this.length(current.next, acum = acum + 1)
+  }
+  return acum
+}
+LinkedList.prototype.print = function(){
+  if (!this.length) {
+    return null;
+  };
+  let current =this.head;
+  let result = "";
+  while(current){
+    result += current.value + "\n";
+    current = current.next;
+  };
+  document.getElementById("getList").value = result;
+}
+
+list = new LinkedList();
+var counter = list.length();
+
+function obtenerDatos(){
+  var text = document.getElementById("data").value;
+  list.append(text);
+  console.log(list.length());
+  list.print();
+}
+
+function Funciono(id){
+  var id = document.getElementById(id);
+  var selected = id.value;
+  console.log(selected);
+  escribe = document.getElementById("texto");
+  escribe.innerHTML = selected;
+}
+
+list.append("Esteban");
+list.append("Tania");
+list.append("Santiago");
+console.log(list.findNode("Esteban"));
+console.log(list.length());
+
